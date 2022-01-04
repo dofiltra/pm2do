034d81d29864a2f523be01a448f347f7961b09f8
Dmitriy <pointsman1@gmail.com>
2022-01-04 09:31:56 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import pm2 from 'pm2'
+import fetch from 'node-fetch'
+import { fileURLToPath } from 'url'
 import { sleep } from 'time-helpers'
 
 type TAppSettings = {}
+type TPm2App = { appName: string; url: string }
 
 class App {
   static version = 1
@@ -15,24 +17,51 @@ class App {
     dotenv.config({ path: path.join(App.rootPath, `.env`) })
   }
 
-  async start() {
-    console.log('start...')
-    await sleep(30e3)
+  async start(): Promise<void> {
+    console.log(`Start 'pm2do' v${App.version} | ${new Date().toJSON()}`)
+    const apps: TPm2App[] = [
+      { appName: 'xfwd', url: 'https://api.dofiltra.com' },
+      // { appName: 'xfwd', url: 'https://api.dofiltra.com' },
+    ]
 
+    await Promise.all(
+      apps.map(async (app) => {
+        if (!(await this.isLiveApp(app))) {
+          this.restartApp(app.appName)
+        }
+      })
+    )
+    await sleep(10e3)
+
+    return this.start()
+  }
+
+  private async restartApp(appName: string) {
     pm2.connect(function (err) {
       if (err) {
         console.error(err)
-        process.exit(2)
+        // process.exit(2)
+        return
       }
 
-      pm2.list((err, list) => {
-        console.log(err, list)
+      pm2.list(async (err, list) => {
+        err && console.log(err)
 
-        pm2.restart('pm2do', (err, proc) => {
+        pm2.restart(appName, (err, proc) => {
           pm2.disconnect()
         })
       })
     })
+  }
+
+  private async isLiveApp(app: TPm2App) {
+    try {
+      const resp = await fetch(app.url)
+      return resp.ok && resp.status === 200
+    } catch (e: any) {
+      console.log(e)
+      return false
+    }
   }
 }
 
