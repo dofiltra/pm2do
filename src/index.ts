7a906caa5cd1ff894ccb3d124e6a357a3bdc279c
Dmitriy <pointsman1@gmail.com>
2022-01-05 18:08:20 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import pm2 from 'pm2'
+import fs from 'fs'
 import fetch from 'node-fetch'
 import { fileURLToPath } from 'url'
 import { sleep } from 'time-helpers'
+import { DateTime } from 'luxon'
 
 type TAppSettings = {}
-type TPm2App = { appName: string; url: string }
+type TPm2App = {
+  appName: string
+  url?: string
+  file?: {
+    path: string
+    maxIdleSeconds?: number
+  }
+}
 
 class App {
-  static version = 3
+  static version = 4
   static env = process.env
   static rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..')
 
@@ -18,52 +27,109 @@ class App {
   }
 
   async start(): Promise<void> {
-    //console.log(`Start 'pm2do' v${App.version} | ${new Date().toJSON()}`)
-    const apps: TPm2App[] = [
-      { appName: 'xfwd', url: 'https://api.dofiltra.com' },
-      // { appName: 'xfwd', url: 'https://api.dofiltra.com' },
-    ]
-
-    await Promise.all(
-      apps.map(async (app) => {
-        if (!(await this.isLiveApp(app))) {
-          this.restartApp(app.appName)
-        }
-      })
-    )
+    console.log(`Start 'pm2do' v${App.version} | ${new Date().toJSON()}`)
     await sleep(10e3)
+
+    pm2.connect((err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      pm2.list(async (err, list) => {
+        if (err) {
+          return console.log(err)
+        }
+
+        const apps: TPm2App[] = [
+          {
+            appName: 'xfwd',
+            url: 'https://api.dofiltra.com',
+          },
+          {
+            appName: 'aiback',
+            file: {
+              path: '/root/.pm2/logs/aiback-out.log',
+              maxIdleSeconds: 15 * 60,
+            },
+          },
+        ].filter((app) => list.some((l) => l.name === app.appName))
+
+        console.log(
+          'Check apps: ',
+          apps.map((x) => x.appName)
+        )
+
+        await Promise.all(
+          apps.map(async (app) => {
+            if (!(await this.isLiveApp(app))) {
+              this.restartApp(app.appName)
+            }
+          })
+        )
+      })
+    })
 
     return this.start()
   }
 
   private async restartApp(appName: string) {
-    console.log(`Restarting... app '${appName}'`)
+    console.log(`Restarting app '${appName}'...`)
 
-    pm2.connect(function (err) {
-      if (err) {
-        console.error(err)
-        // process.exit(2)
-        return
-      }
+    await new Promise((resolve) => {
+      pm2.connect(async (err) => {
+        if (err) {
+          // process.exit(2)
+          return resolve(console.error(err))
+        }
 
-      pm2.list(async (err, list) => {
-        err && console.log(err)
+        pm2.flush(appName, (e) => e && console.log(e))
+        await sleep(5e3)
 
-        pm2.restart(appName, (err, proc) => {
-          pm2.disconnect()
+        pm2.list(async (err, list) => {
+          err && console.log(err)
+
+          pm2.restart(appName, (err, proc) => {
+            resolve(pm2.disconnect())
+          })
         })
       })
     })
+
+    await sleep(15e3)
   }
 
   private async isLiveApp(app: TPm2App) {
     try {
-      const resp = await fetch(app.url)
-      return resp.ok && resp.status === 200
+      const { url, file } = app
+
+      if (url) {
+        const resp = await fetch(url)
+        return resp.ok && resp.status === 200
+      }
+
+      if (file?.path) {
+        const stat = fs.statSync(file.path)
+
+        if (stat?.mtime && file?.maxIdleSeconds) {
+          const { seconds: idleSeconds } = DateTime.fromJSDate(new Date()).diff(
+            DateTime.fromJSDate(stat.mtime),
+            'seconds'
+          )
+
+          // console.log(stat.mtime, idleSeconds, idleSeconds > file.maxIdleSeconds)
+
+          if (idleSeconds > file.maxIdleSeconds) {
+            return false
+          }
+        }
+      }
     } catch (e: any) {
       console.log(e)
       return false
     }
+
+    return true
   }
 }
 
